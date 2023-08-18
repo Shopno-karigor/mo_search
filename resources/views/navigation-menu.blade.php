0f931f347d683c8075a6bf42fd8b0d86c3659287
unknown <karigor93@gmail.com>
2023-08-18 12:11:31 +0600

    Jetstrim installed

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,10 +13,7 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('My Account') }}
-                    </x-nav-link>
-                    <x-nav-link href="{{ route('/') }}">
-                        {{ __('OMS Admin') }}
+                        {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
             </div>
